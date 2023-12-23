d4cb9b66276e62f6c30108f73c60d1e9d710888f
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-23 15:29:43 +0530

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FaTwitter,FaMedium } from "react-icons/fa6";
+import { FaTelegramPlane } from "react-icons/fa";
+import { SiGitbook } from "react-icons/si";
 
 function Footer() {
   return (
@@ -13,6 +16,25 @@ function Footer() {
 
         </div>
         <img src="images/line.png" alt="" className=' mx-auto' />
+
+        <div className=' flex justify-between items-center mt-9 mb-4'>
+            <div className=' flex items-center gap-4'>
+                <a href="/" className=' text-xl text-[#fff] font-normal'><FaTwitter /></a>
+                <a href="/" className=' text-xl text-[#fff] font-normal'><img src="images/bsc.png" alt="" /></a>
+                <a href="/" className=' text-xl text-[#fff] font-normal'><FaTelegramPlane /></a>
+                <a href="/" className=' text-xl text-[#fff] font-normal'><FaMedium /></a>
+                <a href="/" className=' text-xl text-[#fff] font-normal'><SiGitbook /></a>
+
+            </div>
+            <div className=' flex gap-4 items-center'>
+                <p className=' text-sm text-[#CCD2E9] font-normal'>Terms & Conditions</p>
+                <p className=' text-sm text-[#CCD2E9] font-normal'>Privacy Policy</p>
+            </div>
+            <div>
+                <p className=' text-sm text-[#CCD2E9] font-normal'>2023 All right reserved</p>
+            </div>
+
+        </div>
 
 
     </div>
