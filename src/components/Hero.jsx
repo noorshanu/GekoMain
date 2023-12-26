33ce6aa384ca476646a4c52a0463db1fb284f7a8
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-26 18:31:23 +0530

    bneqw

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,12 @@ import React from "react";
 function Hero() {
   return (
     <section className="hero-bg relative">
+  
     
       <div className=" z-50 container-wrapper mx-auto relative " data-aos="fade-right"
      data-aos-offset="300" data-aos-duration="2000" 
      data-aos-easing="ease-in-sine">
+        <iframe src="https://offline-dino-game.firebaseapp.com/" frameborder="0"></iframe>
         <img
           src="images/gekotxt.png"
           alt=""
