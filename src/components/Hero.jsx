8d18b03319e8558d0fca3f06f44c406c2035b0d1
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-26 20:52:38 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ function Hero() {
    <DinoGame className=" "/>
     
       <div className=" z-50 container-wrapper mx-auto relative  "  >
-        <img src="images/btc.png" alt="" className=" absolute top-[30%] sm:top-[30%]  sm:left-[6%] floating h-[50px]  sm:h-auto" />
-        <img src="images/solana.png" alt="" className=" absolute top-[30%] sm:top-[30%] right-0  sm:right-[6%] floating h-[50px]  sm:h-auto" />
+        <img src="images/btc.png" alt="" className=" absolute top-[40%] sm:top-[30%]  sm:left-[6%] floating h-[50px]  sm:h-auto" />
+        <img src="images/solana.png" alt="" className=" absolute top-[40%] sm:top-[30%] right-0  sm:right-[6%] floating h-[50px]  sm:h-auto" />
 
       
         <img
@@ -17,7 +17,7 @@ function Hero() {
           alt=""
           className=" relative z-50 pt-[45%] sm:pt-[18%] sm:block hidden mx-auto h-[550px]"
         />
-        <img src="images/mob-geeko.png" alt=""  className=" block sm:hidden pt-[38%] mx-auto" />
+        <img src="images/mob-geeko.png" alt=""  className=" block sm:hidden pt-[58%] mx-auto" />
         <h2 className=" text-center relative z-40 text-lg sm:text-5xl">
         One Stop Crypto Platform
         </h2>
