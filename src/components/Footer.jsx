38389585e36d2e9f024d5c8addb36707ad5b91ba
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-08 22:36:58 +0530

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,17 +18,17 @@ function Footer() {
         <img src="images/line.png" alt="" className=' mx-auto' />
 
         <div className=' flex justify-between items-center mt-9 mb-4 flex-col sm:flex-row gap-2'>
-            <div className=' flex items-center gap-4'>
-                <a href="https://x.com/geekocoin" className=' text-xl text-[#fff] font-normal'><FaTwitter /></a>
-                <a href="/" className=' text-xl text-[#fff] font-normal'><img src="images/bsc.png" alt="" /></a>
+            <div className=' flex items-center gap-4 z-[99999]'>
+                <a href="https://x.com/geekocoin" className=' text-xl text-[#fff] font-normal cursor-pointer'><FaTwitter /></a>
+                <a href="/" className=' text-xl text-[#fff] font-normal cursor-pointer'><img src="images/bsc.png" alt="" /></a>
                 <a href="https://t.me/geekocoin" className=' text-xl text-[#fff] font-normal'><FaTelegramPlane /></a>
-                <a href="/" className=' text-xl text-[#fff] font-normal'><FaMedium /></a>
-                <a href="/" className=' text-xl text-[#fff] font-normal'><SiGitbook /></a>
+                <a href="/" className=' text-xl text-[#fff] font-normal cursor-pointer'><FaMedium /></a>
+                <a href="/" className=' text-xl text-[#fff] font-normal cursor-pointer'><SiGitbook /></a>
 
             </div>
-            <div className=' flex gap-4 items-center'>
-                <p className=' text-sm text-[#CCD2E9] font-normal'>Terms & Conditions</p>
-                <p className=' text-sm text-[#CCD2E9] font-normal'>Privacy Policy</p>
+            <div className=' flex gap-4 items-center z-50'>
+                <p className=' text-sm text-[#CCD2E9] font-normal cursor-pointer'>Terms & Conditions</p>
+                <p className=' text-sm text-[#CCD2E9] font-normal cursor-pointer'>Privacy Policy</p>
             </div>
             <div>
                 <p className=' text-sm text-[#CCD2E9] font-normal'>2023 All right reserved</p>
